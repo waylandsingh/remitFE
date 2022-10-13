cc5c9536dc497c6b0d184d5f06829f3e60d33a7d
waylandsingh <wmsingh@ucdavis.edu>
2022-10-12 21:10:09 -0700

    basic test of tailwind + vite bundler

---
+++ src/components/index.jsx
@@ -0,0 +1,14 @@
+// placeholder for making the import process easier for App.jsx
+
+// import the components that you'd like to `import ___ from 'components`
+import Hero from './Hero';
+import Navbar from './Navbar';
+
+
+
+// export them all in 1 object, each of the named objects will be available for import with similar syntax
+
+export {
+    Hero, 
+    Navbar
+}
