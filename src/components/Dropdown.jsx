556bfdb6425a7e2102af21dc7889d695068da7b6
waylandsingh <wmsingh@ucdavis.edu>
2022-10-20 22:00:12 -0700

    intermediate commit working on dropdown mvp

---
+++ src/components/Dropdown.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+// change into a slide-down menu?
+const Dropdown = () => {
+    return (
+        <div className="absolute right-0">
+            Dropdown menu that get's hiddden
+            <br />
+            asdkajd[}and
+            d
+            </div>
+    )
+}
+
+export default Dropdown
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,23 @@
 import React from 'react'
+import Dropdown from './Dropdown'
+
 
 const Navbar = () => {
+
+    const [dropdownVisible, setDropdownVisible] = React.useState(0)
+    
+
     return (
-        <nav className = "flex justify-between align-center p-3  bg-black rounded-md">
-            this is a navbar, man!
-            <ul className="flex flex-row justify-between">
-                <li className="font-normal cursor-pointer text-[16px] mr-10">Home</li>
-                <li className="font-normal cursor-pointer text-[16px] mr-10">Login/Signup</li>
-                <li className="font-normal cursor-pointer text-[16px] mr-0">sidebar Icon</li>
-            </ul>
+        <nav >
+            <div className = "flex justify-between align-center p-3  bg-black rounded-md">
+                this is a navbar, man!
+                <ul className="flex flex-row justify-between">
+                    <li className="font-normal cursor-pointer text-[16px] mr-10">Home</li>
+                    <li className="font-normal cursor-pointer text-[16px] mr-10">Login/Signup</li>
+                    <li className="font-normal cursor-pointer text-[16px] mr-0" >sidebar Icon</li>
+                </ul>
+            </div>
+            <Dropdown />
         </nav>
     )
 }
